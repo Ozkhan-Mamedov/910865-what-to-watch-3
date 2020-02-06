69da73f3239bf583d1430085ae5ee9781567a5f3
Ozkhan Mamedov <ozhanm96@yandex.ru>
2020-02-07 01:31:44 +0400

    Небольшой рефакторинг

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import App from "./components/app";
-
-const promoFilmData = {
-  name: `The Grand Budapest Hotel`,
-  genre: `Drama`,
-  releaseDate: `2014`,
-};
+import App from "./components/app/app";
+import promoFilmData from "./mock/promo";
 
 ReactDOM.render(<App promoFilmData={promoFilmData} />, document.getElementById(`root`));
