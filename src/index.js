eeba3d5bc1b9d75e84bdf8f0ed82ca448f85e4d7
Ozkhan Mamedov <ozhanm96@yandex.ru>
2020-02-07 01:52:11 +0400

    Замокал названия фильмов и передал их через пропсы

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import App from "./components/app/app";
-import promoFilmData from "./mock/promo";
 
-ReactDOM.render(<App promoFilmData={promoFilmData} />, document.getElementById(`root`));
+import App from "./components/app/app";
+
+import promoFilmData from "./mock/promo";
+import films from "./mock/films";
+
+ReactDOM.render(<App promoFilmData={promoFilmData} films={films} />, document.getElementById(`root`));
---
+++ src/mock/films.js
@@ -0,0 +1,14 @@
+const films = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `What We Do in the Shadows`,
+  `Shutter Island`,
+  `Pulp Fiction`,
+  `No Country for Old Men`,
+  `Seven Years in Tibet`,
+  `Dardjeeling Limited`
+];
+
+export default films;
