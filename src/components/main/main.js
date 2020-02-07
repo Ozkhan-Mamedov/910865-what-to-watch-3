8ce87e69f376652936c61f253e7e41da5749232a
Ozkhan Mamedov <ozhanm96@yandex.ru>
2020-02-07 22:26:13 +0400

    Правки по замечаниям к cbb6b13

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -101,14 +101,14 @@ const Main = (props) => {
           </ul>
 
           <div className="catalog__movies-list">
-            {films.map((it) =>
-              <article key={it} className="small-movie-card catalog__movies-card">
+            {films.map((film) =>
+              <article key={film} className="small-movie-card catalog__movies-card">
                 <div className="small-movie-card__image">
                   <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg"
-                    alt={it} width="280" height="175"/>
+                    alt={film} width="280" height="175"/>
                 </div>
                 <h3 className="small-movie-card__title">
-                  <a className="small-movie-card__link" href="movie-page.html">{it}</a>
+                  <a className="small-movie-card__link" href="movie-page.html">{film}</a>
                 </h3>
               </article>
             )}
