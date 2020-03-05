7baab1af4805e5c3a4e6ae4d596699a37cac8b7c
Ozkhan Mamedov <ozhanm96@yandex.ru>
2020-03-05 17:11:08 +0300

    Установил axios и redux-thunk

--- src/mocks/promo.js
+++ src/mocks/promo.js
@@ -2,8 +2,8 @@ const promoFilmData = {
   name: `The Grand Budapest Hotel`,
   genre: `Drama`,
   releaseDate: `2014`,
-  src: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
-  runTime: 52.209,
+  src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  runTime: 33.016,
 };
 
 export default promoFilmData;
